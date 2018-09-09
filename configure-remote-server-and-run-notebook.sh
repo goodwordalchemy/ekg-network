925c642fe38e5f1ac32c1456884bd2d36e3d9653
David Goldberg <goodwordachemy@gmail.com>
2018-09-09 12:31:06 -0500

    updated remote server startup and configuration scripts

--- configure-remote-server-and-run-notebook.sh
+++ configure-remote-server-and-run-notebook.sh
@@ -1,27 +1,36 @@
 export INSTANCE_NAME="ekg-network"
-export MACHINE_TYPE="n1-standard-16"
+export MACHINE_TYPE="n1-standard-8"
 
-export GPU_STARTUP_WAIT_TIME=10
+export ENABLE_CREATING_REMOTE_SERVER=true
+export GPU_STARTUP_WAIT_TIME=30
+export RUN_AT_STARTUP_SCRIPT="run-at-startup-on-remote-server.sh"
 
-echo "Starting Google Cloud Compute instance that has a GPU attached..."
-gcloud compute instances create $INSTANCE_NAME \
-    --machine-type $MACHINE_TYPE
-    --accelerator type=nvidia-tesla-k80,count=1 \
-    --image-family ubuntu-1604-lts --image-project ubuntu-os-cloud \
-    --maintenance-policy TERMINATE --restart-on-failure \
-    --metadata startup-script='#!/bin/bash
-    echo "Checking for CUDA and installing."
-    # Check for CUDA and try to install.
-    if ! dpkg-query -W cuda-9-0; then
-      curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-      dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
-      apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
-      apt-get update
-      apt-get install cuda-9-0 -y
-    fi'
+set -e 
 
-echo "Waiting $GPU_STARTUP_WAIT_TIME for GPU to attach..."
-sleep $GPU_STARTUP_WAIT_TIME
+if $ENABLE_CREATING_REMOTE_SERVER; then
+	echo "Starting Google Cloud Compute instance that has a GPU attached..."
+	gcloud compute instances create $INSTANCE_NAME \
+		--machine-type $MACHINE_TYPE \
+		--accelerator type=nvidia-tesla-k80,count=1 \
+		--image-family ubuntu-1604-lts --image-project ubuntu-os-cloud \
+		--maintenance-policy TERMINATE --restart-on-failure \
+		--metadata startup-script='#!/bin/bash
+		echo "Checking for CUDA and installing."
+		# Check for CUDA and try to install.
+		if ! dpkg-query -W cuda-9-0; then
+		  curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+		  dpkg -i ./cuda-repo-ubuntu1604_9.0.176-1_amd64.deb
+		  apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+		  apt-get update
+		  apt-get install cuda-9-0 -y
+		fi'
+
+	echo "Waiting $GPU_STARTUP_WAIT_TIME seconds for GPU to attach..."
+	sleep $GPU_STARTUP_WAIT_TIME
+fi
 
 echo "Running startup script on remote server"
-gcloud compute ssh $INSTANCE_NAME --command 'bash ./run-at-startup-on-remote-server.sh'
+gcloud compute scp ./$RUN_AT_STARTUP_SCRIPT ekg-network:~/$RUN_AT_STARTUP_SCRIPT
+echo "Finished copying startup script to remote server"
+
+gcloud compute ssh $INSTANCE_NAME --command "source $RUN_AT_STARTUP_SCRIPT" -- -L 8888:localhost:8888
--- run-at-startup-on-remote-server.sh
+++ run-at-startup-on-remote-server.sh
@@ -1,11 +1,18 @@
+export MINICONDA_PATH_COMMAND="export PATH=\"/home/davidgoldberg/miniconda3/bin:\$PATH\""
+echo "Waiting for backgroud startup processes to stop."
+wait 
+
 echo "Installing miniconda3"
-wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash miniconda.sh -b
-echo "export PATH=\"/home/davidgoldberg/miniconda3/bin:\$PATH\"". > ~/.bashrc
-source ~/.bashrc
+echo $MINICONDA_PATH_COMMAND >> ~/.bashrc
+export PATH="/home/davidgoldberg/miniconda3/bin:$PATH"
+
 
 echo "Installing python-based data science tools"
 conda create -n tensorflow pip python=3.5
+wait
+
 source activate tensorflow
 
 pip install tensorflow keras ipython notebook matplotlib
