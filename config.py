2357388ba8e4fd5f9b27ade15922bcdd4abf4b20
David Goldberg <goodwordachemy@gmail.com>
2018-10-08 13:35:05 -0500

    ability to specify config files in a folder

--- config.py
+++ config.py
@@ -1,9 +1,24 @@
+import argparse
 import yaml
 
-CONFIG_FILENAME = 'config.yaml'
+DEFUALT_CONFIG_FILENAME = 'config.yaml'
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--config', default=DEFUALT_CONFIG_FILENAME)
+
+    args = parser.parse_args()
+
+    return args
+
 
 def get_config():
-    with open(CONFIG_FILENAME, 'r') as f:
+    args = get_args()
+
+    config_filename = args.config
+
+    with open(config_filename, 'r') as f:
         config = yaml.load(f)
 
     return config
