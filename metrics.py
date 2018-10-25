b3a74a979d98372291877c43da4fb1cd9d42106a
David Goldberg <goodwordachemy@gmail.com>
2018-10-25 17:14:16 -0400

    use keras metrics package

--- metrics.py
+++ metrics.py
@@ -1,20 +1,3 @@
-import functools
-from keras import backend as K
-import tensorflow as tf
+from keras_metrics import f1_score, precision, recall
 
-def as_keras_metric(method):
-    @functools.wraps(method)
-    def wrapper(self, *args, **kwargs):
-        """ Wrapper for turning tensorflow metrics into keras metrics """
-        value, update_op = method(self, *args, **kwargs)
-        K.get_session().run(tf.local_variables_initializer())
-        with tf.control_dependencies([update_op]):
-            value = tf.identity(value)
-        return value
-    return wrapper
-
-precision = as_keras_metric(tf.metrics.precision)
-recall = as_keras_metric(tf.metrics.recall)
-f1_score = as_keras_metric(tf.contrib.metrics.f1_score)
-
-all_metrics = [f1_score, precision, recall]
+all_metrics = [f1_score(label=1), precision(label=1), recall(label=1)]
